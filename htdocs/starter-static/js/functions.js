bea1133526b918d82fbc36e9aa601f20c15854c4
Roy Barber <roybarberuk@gmail.com>
2014-11-24 16:28:33 +0000

    Added starter static template

---
+++ htdocs/starter-static/js/functions.js
@@ -0,0 +1,26 @@
+var win = $(window),
+	doc = $(document),
+	body = $('body'),
+	$self = $(this);
+
+$(function(){
+
+	body.addClass('red');
+	
+    // Your JS Here
+    
+    // Sample Simple Function
+    //$('.class').action();
+    
+    // Sample Hover Function
+	//$('.class').hover(function(){
+    //    $(this).action();
+	//});
+	
+	// Sample Click Function
+	//$('#dropnav').click(function(e) {
+	//	$(this).action();
+	//	e.preventDefault();
+	//});
+		
+});
